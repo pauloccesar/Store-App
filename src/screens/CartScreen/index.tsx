266c1a49f37f86b6837da310e12823ece8774e76
pauloccesar <pauloccesar.131@gmail.com>
2022-08-06 16:07:43 -0300

    create cartScreen

--- src/screens/CartScreen/index.tsx
+++ src/screens/CartScreen/index.tsx
@@ -1,23 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { useRoute } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { FlatList } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, ContainerList, Header, Icon, ImageList, Title, TitleList, TouchableOpacity } from './styles';
 
-export default function CartScreen() {
+export default function CartScreen({ navigation }: any) {
+  const [cartList, setCartList]: any = useState([]);
+
+  useEffect(() => {
+    getCartList()
+  }, []);
+
+  async function getCartList() {
+    AsyncStorage.getItem('cartList').then((value: any) => {
+      const accounts = JSON.parse(value);
+      setCartList(accounts)
+    });
+  }
+
+  async function handleRemoveProduct(id: number) {
+    const updatedCartList = cartList.filter((cartList: { id: number; }) => cartList.id !== id);
+    try {
+      let newProduct: any = [];
+      await AsyncStorage.getItem('cartList').then((value: any) => {
+        console.log(JSON.parse(value));
+        newProduct = JSON.parse(value)
+        newProduct.push(updatedCartList)
+      });
+      AsyncStorage.setItem('cartList', JSON.stringify(newProduct));
+      navigation.navigate('Cart');
+      setCartList(updatedCartList);
+    } catch (error) {
+    }
+  }
+
+  // const Delete = () => AsyncStorage.setItem('cartList', JSON.stringify([]));
+
+
+  console.log("produto aqui", cartList)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Header>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Home')}>
+          <Icon name="arrow-left" style={{ paddingLeft: 10 }} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Cart')}>
+          <Icon name="shopping-cart" />
+        </TouchableOpacity>
+      </Header>
+      <Title>{`produtos adicionados:`}</Title>
+      <FlatList
+        data={cartList}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <ContainerList>
+            <ImageList source={{ uri: item?.image }} />
+            <TitleList>{item?.name}</TitleList>
+            <TouchableOpacity
+              onPress={() => handleRemoveProduct(item?.id)}>
+              <Icon name="trash-2" />
+            </TouchableOpacity>
+          </ContainerList>
+        )
+        }
+      />
+    </Container >
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/CartScreen/styles.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Feather } from '@expo/vector-icons';
+
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 50px;
+  flex-direction: row;
+  background-color: #00d900;
+  align-items:  center;
+  justify-content: space-between;
+`;
+
+export const Icon = styled(Feather)`
+  color: #000000;
+  font-size: 25px;
+  padding-right: 15px;
+`;
+
+export const Title = styled.Text`
+  font-size: 14px;
+  padding-left: 15px;
+`;
+
+export const TouchableOpacity = styled.TouchableOpacity`
+`;
+
+export const ContainerList = styled.View`
+  flex: 1;
+  width: 95%;
+  height: 60px;
+  flex-direction: row;
+  background-color: #99d900;
+  border-radius: 10px;
+  align-items:  center;
+  justify-content:space-between;
+  margin: 5px;
+`;
+
+export const ImageList = styled.Image`
+  width: 45px;
+  height: 45px;
+  border-radius: 10px;
+`;
+
+export const TitleList = styled.Text`
+  max-width: 70%;
+  font-size: 14px;
+  padding-left: 15px;
+`;
